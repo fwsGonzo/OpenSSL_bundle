733533fe071cc59da1efae6c3d48a83c16d2823b
fwsGonzo <fwsgonzo@hotmail.com>
2018-04-09 13:18:25 +0200

    Fix not copying libraries properly

--- create_bundle.sh
+++ create_bundle.sh
@@ -4,7 +4,7 @@ LOCAL_BUILD=local_build
 
 mkdir -p $BUNDLE_FOLDER/include
 mkdir -p $BUNDLE_FOLDER/lib
-cp $LOCAL_BUILD/*.a $BUNDLE_FOLDER/lib
+cp $LOCAL_BUILD/lib/*.a $BUNDLE_FOLDER/lib
 cp -r $LOCAL_BUILD/include/openssl $BUNDLE_FOLDER/include
 tar -czvf openssl_bundle.tar.gz $BUNDLE_FOLDER/
 echo Removing temp folder $BUNDLE_FOLDER
