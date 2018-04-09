8eb5e7b2f78057e5a8424e80ba6bcd9586cfcfd1
fwsGonzo <fwsgonzo@hotmail.com>
2018-04-09 13:07:50 +0200

    Update bundle creation script

--- create_bundle.sh
+++ create_bundle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 BUNDLE_FOLDER=openssl_bundle
+LOCAL_BUILD=local_build
+
 mkdir -p $BUNDLE_FOLDER/include
-mkdir -p $BUNDLE_FOLDER/openssl
-cp /usr/lib/x86_64-linux-gnu/libssl.a $BUNDLE_FOLDER/
-cp /usr/lib/x86_64-linux-gnu/libcrypto.a $BUNDLE_FOLDER/
-cp -r /usr/include/openssl $BUNDLE_FOLDER/include
-cp /usr/include/x86_64-linux-gnu/openssl/opensslconf.h $BUNDLE_FOLDER/openssl
+mkdir -p $BUNDLE_FOLDER/lib
+cp $LOCAL_BUILD/*.a $BUNDLE_FOLDER/lib
+cp -r $LOCAL_BUILD/include/openssl $BUNDLE_FOLDER/include
 tar -czvf openssl_bundle.tar.gz $BUNDLE_FOLDER/
+echo Removing temp folder $BUNDLE_FOLDER
+rm -rf $BUNDLE_FOLDER
